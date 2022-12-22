25a8a744425269644607e1c822179e629098ea85
Dmitri Gaponenko <dm.gaponenko@gmail.com>
2022-12-23 00:31:08 +0300

    feat: add dotenv and http sample server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
+import 'dotenv/config';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
 const message = 'Hello crud api!';
+const users = [
+  {
+    id: '1',
+    username: 'testUser',
+    age: '33',
+    hobbies: ['dance', 'chess'],
+  },
+];
 
 console.log(message);
+
+const PORT = process.env.PORT || 4000;
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(users));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}/`);
+});
